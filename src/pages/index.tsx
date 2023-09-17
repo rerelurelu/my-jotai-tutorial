6ae923615871be2c67bdeb4dfed39400f133e10e
rerelurelu <awaawapurin@gmail.com>
2023-09-18 03:22:50 +0900

    👻

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import { countAtom } from '@/atom/countAtom';
+import { Box, Button } from '@chakra-ui/react';
+import { useAtom } from 'jotai';
+
 export default function Home() {
+  const [count, setCount] = useAtom(countAtom);
+  const increment = () => {
+    setCount((value) => value + 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Box
+      w="100vw"
+      pt={10}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      flexDir={'column'}
+      gap={4}
+    >
+      <p>{count}</p>
+      <Button onClick={increment}>+</Button>
+      <Button onClick={reset}>Reset</Button>
+    </Box>
   );
 }
